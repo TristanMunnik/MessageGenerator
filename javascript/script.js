c013715a1672bf2a37ec4700abaefce1e01d843e
TMunnik <tristanmunnik3@gmail.com>
2025-12-10 18:46:20 +0200

    Cleaned up the final code

--- javascript/script.js
+++ javascript/script.js
@@ -8,15 +8,26 @@ const moodAdvice = {
     bored : ['practice coding', 'play some games', 'go to the beach if you can']
 }
 
+// function to get a random mood
 function getMood () {
     const mood = Object.keys(moodAdvice);
     const randomIndex = Math.floor(Math.random() * mood.length);
     return mood[randomIndex];
 }
 
+// function to get advice based on mood
 function getAdvice(mood) {
     const advice = moodAdvice[mood];
     const randomIndex = Math.floor(Math.random() * advice.length);
     return advice[randomIndex];
 }
 
+// function for displaying mood and advice
+function displayMoodAndAdvice() {
+    const randomMood = getMood();
+    const randomAdvice = getAdvice(randomMood);
+
+    console.log(`You are ${randomMood}. You should ${randomAdvice}!`);
+}
+
+displayMoodAndAdvice();
