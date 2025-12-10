8d8bf7dce9e13daf7af844eb6e3787c964da556a
TMunnik <tristanmunnik3@gmail.com>
2025-12-10 17:14:39 +0200

    Fixed folder issue with github

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+
+// object for moods and advice
+const moodAdvice = {
+    happy : ['spread the positivity', 'keep smiling', 'try coding'],
+    sad : ['talk to a friend', 'take a walk', 'check the sunset out'],
+    angry : ['go for a walk', 'listen to music', 'play some games'],
+    tired : ['take a nap', 'drink some water', 'do some exercise'],
+    bored : ['practice coding', 'play some games', 'go to the beach if you can']
+}
